2a86f07ffecb0d5cdf8b7df7887e833f2a592139
Hemchand Pidikiti <hemchand200074@gmail.com>
2021-07-01 12:19:07 +0530

    Redefined styles of web pages and added new component 'skills'.

--- projects/models.py
+++ projects/models.py
@@ -7,3 +7,7 @@ class Project(models.Model):
     description=models.TextField(blank=True)
     def __str__(self):
         return self.summary
+class Skill(models.Model):
+    skill_name=models.CharField(max_length=100)
+    def __str__(self):
+        return self.skill_name
--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Project
+from .models import Skill
 # Create your views here.
 def homepage(request):
     projects=Project.objects
-    return render(request,'projects/home.html',{'projects':projects})
+    skills=Skill.objects
+    return render(request,'projects/home.html',{'projects':projects,'skills':skills},)
 def detail(request,project_id):
     project_detail=get_object_or_404(Project,pk=project_id)
     return render(request,'projects/detail.html',{'project':project_detail})
